143b21b186ddb2d2d9d617f23a44a6ba7e9c0011
Samuel Painter <sam.foy.painter@gmail.com>
2012-02-19 00:42:16 -0500

    Added javascript to toggle on click

--- js/eyg.js
+++ js/eyg.js
@@ -1,15 +1,53 @@
 $(function(){
+    $('.off').hide();
+    var is_on = true;
     var $write = $('#write'),
         shift = false,
         capslock = false;
+    
+    var top_left = new Array("g","h","i","f","","j");
+    var top_mid = new Array("l","m","n","k","","o");
+    var top_right = new Array("q","r","s","p","","t");
+    var bot_left = new Array("b","c","d","a","","e");
+    var bot_mid = new Array("","","","","","");
+    var bot_right = new Array("v","w","x","u","y","z");
 
-    $('#keyboard .inner').click(function(){
-        var $this = $(this),
-            character = $this.html();
+    $('#keyboard .outer').click(function(){
+        if (is_on) {
+            //$('.off').animate({
+                //'opacity' : 'toggle',
+            //});
+            //$('.on').animate({
+                //'opacity' : 'toggle',
+            //});
+            $('.off').show();
+            $('.on').hide();
+            is_on = false;
+        } else {
+            //$('.off').animate({
+                //'opacity' : 'toggle',
+            //});
+            //$('.on').animate({
+                //'opacity' : 'toggle',
+            //});
+            $('.off').hide();
+            $('.on').show();
+            is_on = true;
+        }
+            
+
         
-    console.log($this.html());
-        $write.html($write.html() + character);
+        
+        
+    //$('#keyboard .inner').click(function(){
+        //var $this = $(this),
+            //character = $this.html();
+        
+    //console.log($this.html());
+        //$write.html($write.html() + character);
     });
+
+
         
 });
     
